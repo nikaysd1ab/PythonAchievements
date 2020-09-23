593c79dfee08b6c68372f6681d92c50a75d8212a
nikay <nikayklidas@gmail.com>
2020-09-23 12:08:23 +0200

    tellenmetpy.py

---
+++ tellenmetpy.py
@@ -0,0 +1,20 @@
+puntgetal = 6
+
+waarde1 = puntgetal + 2
+waarde2 = puntgetal - 2
+waarde3 = puntgetal % 2
+waarde4 = puntgetal / 6
+
+getal1 = 10;
+getal1 = getal1 + 6
+print(getal1)
+
+waarde4 += 6
+print(getal1)
+
+
+
+print(waarde1)
+print(waarde2)
+print(waarde3)
+print(waarde4)
