dcc69c0c6479e439194746bb56f69203d45b8e30
nikay <nikayklidas@gmail.com>
2020-10-28 12:38:46 +0100

    keuzens.py

---
+++ keuzens.py
@@ -0,0 +1,53 @@
+print("je werker gaat")
+choice = input()
+if choice == 'sta op':
+    print("je gaat je bed uit ")
+elif choice == 'nog5minuten':
+    print(" zzzzzzzzzzzzzzzzzzzzzzzzzz")
+else:
+    print(choice, "voer wat in lul")
+
+print ("wat doe je eerst hond uit laten of kofie ")
+
+choice = input()
+if choice == 'laat de hond uit':
+    print("je laat de hond uit ")
+elif choice == 'kofie':
+    print(" je zet een bak kofie maar de hond houd het niet meer en schijt op nde grond")
+else:
+    print(choice, "bene gij echt zo dom ")
+
+print("ga naar school of fuck it en blijf tuis")
+
+choice = input()
+if choice == 'ga naar school':
+    print("je gaat naar school ")
+elif choice == 'fuck it':
+    print(" je gaat niet naar school")
+else:
+    print(choice, "ja rot nou maar op met je kak hoofd")
+
+
+print("ga op de fiets naar het stasion of bus")
+
+choice = input()
+if choice == 'fiets':
+    print("ik pak me fiets ")
+elif choice == 'fuck it':
+    print(" pak de bus")
+else:
+    print(choice, "ja rot nou maar op met je kak hoofd")
+
+
+
+print("loop je naar school of pak je de bus ")
+
+choice = input()
+if choice == 'bus':
+    print("je pakt de bus  ")
+elif choice == 'lopen ':
+    print(" je loopt naar school")
+else:
+    print(choice, "voer wat in")
+
+
