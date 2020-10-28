808e4bd6e99735fd58effe76b221faedae4c3135
nikay <nikayklidas@gmail.com>
2020-10-28 12:42:03 +0100

    keuzens.py

---
+++ shoppinglis.py
@@ -0,0 +1,5 @@
+list = ("aardbij apple kaas brood salami")
+shoppingcart = ("aardbij apple kaas brood")
+
+if shoppingcart == list :print("done")
+else :print("continuw shopping")
--- tellenmetpy.py
+++ tellenmetpy.py
@@ -4,17 +4,29 @@ waarde1 = puntgetal + 2
 waarde2 = puntgetal - 2
 waarde3 = puntgetal % 2
 waarde4 = puntgetal / 6
+waarde8 = puntgetal * 6
 
-getal1 = 10;
-getal1 = getal1 + 6
-print(getal1)
-
+getal1 = 10
 waarde4 += 6
 print(getal1)
 
+waarde5 = 12
+waarde5 -= 6
+
+
+waarde6 = 12
+waarde6 /= 6
+
+waarde7 = 12
+waarde7 %= 6
 
 
 print(waarde1)
 print(waarde2)
 print(waarde3)
 print(waarde4)
+print(waarde5)
+print(waarde6)
+print(waarde7)
+print(getal1)
+print(waarde8)
